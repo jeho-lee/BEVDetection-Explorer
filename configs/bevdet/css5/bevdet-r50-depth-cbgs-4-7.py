3ee60c1afa36a46751a0373e055dfd601d1e94f4
jeho-lee <jeholee@yonsei.ac.kr>
2023-04-08 21:38:48 +0900

    training configs

--- configs/bevdet/css5/bevdet-r50-depth-cbgs-4-7.py
+++ configs/bevdet/css5/bevdet-r50-depth-cbgs-4-7.py
@@ -13,7 +13,7 @@ Configuraion Change History
 
 Current Change
 
-2023-4-7 (ai datacenter)
+2023-4-7 (css5)
 - ConvNeXt-base backbone (from SOLOFusion)
 - Batch size per GPU: 4 (due to memory limit)
 - lr: 0.0001
@@ -198,8 +198,7 @@ model = dict(
 
 # Data
 dataset_type = 'NuScenesDataset'
-data_root = '/datasets/nuscenes/' # AI datacenter
-ann_root = '/home/dlwpgh1994/3D-perception/data/'
+data_root = '/data/home/jeholee/omni3D/data/nuscenes/' # TODO
 file_client_args = dict(backend='disk')
 
 bda_aug_conf = dict(
@@ -280,7 +279,7 @@ share_data_config = dict(
 test_data_config = dict(
     pipeline=test_pipeline,
     data_root=data_root,
-    ann_file=data_root + 'nuscenes_infos_val.pkl')
+    ann_file=data_root + 'bevdetv2-nuscenes_infos_val.pkl')
 
 num_gpu = 8
 batch_size_per_device = 4 # due to the memory limitation
@@ -293,7 +292,7 @@ data = dict(
         type='CBGSDataset',
         dataset=dict(
         data_root=data_root,
-        ann_file=data_root + 'nuscenes_infos_train.pkl',
+        ann_file=data_root + 'bevdetv2-nuscenes_infos_train.pkl',
         pipeline=train_pipeline,
         classes=class_names,
         test_mode=False,
